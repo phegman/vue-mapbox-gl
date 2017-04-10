2924bb6c29cf8a9119f7469f3b47791f3e275ec2
Peter Hegman <phegman@unionstmedia.com>
2017-04-09 23:18:09 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,80 @@
+Vue.component('mapbox', require('./components/Mapbox.vue'));
+
+const app = new Vue({
+	el: '#app',
+	methods: {
+		mapLoaded(map) {
+			map.addLayer({
+				'id': 'points',
+				'type': 'symbol',
+				'source': {
+					'type': 'geojson',
+					'data': {
+						'type': 'FeatureCollection',
+						'features': [{
+							'type': 'Feature',
+							'geometry': {
+								'type': 'Point',
+								'coordinates': [-77.03238901390978, 38.913188059745586]
+							},
+							'properties': {
+								'title': 'Mapbox DC',
+								'icon': 'monument'
+							}
+						}, {
+							'type': 'Feature',
+							'geometry': {
+								'type': 'Point',
+								'coordinates': [-122.414, 37.776]
+							},
+							'properties': {
+								'title': 'Mapbox SF',
+								'icon': 'harbor'
+							}
+						}]
+					}
+				},
+				'layout': {
+					'icon-image': '{icon}-15',
+					'text-field': '{title}',
+					'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+					'text-offset': [0, 0.6],
+					'text-anchor': 'top'
+				}
+			});
+		},
+		mapClicked(map, e) {
+			this.addPopUp(map, e);
+		},
+		mapMouseMoved(map, e) {
+			const features = map.queryRenderedFeatures(e.point, { layers: ['points'] });
+			map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+		},
+		addPopUp(map, e) {
+			const features = map.queryRenderedFeatures(e.point, { layers: ['points'] });
+			if (!features.length) {
+				return;
+			}
+
+			const feature = features[0];
+
+			const popupContent = Vue.extend({
+				template: '<button @click="popupClicked">Click Me!</button>',
+				methods: {
+					popupClicked() {
+						alert('Popup Clicked!');
+					},
+				}
+			});
+
+			// Populate the popup and set its coordinates
+			// based on the feature found.
+			const popup = new mapboxgl.Popup()
+				.setLngLat(feature.geometry.coordinates)
+				.setHTML('<div id="vue-popup-content"></div>')
+				.addTo(map);
+
+			new popupContent().$mount('#vue-popup-content');
+		}
+	}
+});
---
+++ src/components/Mapbox.js
@@ -0,0 +1,318 @@
+export default {
+	template: '<div id="map"></div>',
+	data () {
+		return {
+
+		};
+	},
+	props: {
+		accessToken: {
+			type: String,
+			required: true
+		},
+		mapOptions: {
+			type: Object,
+			required: true
+		},
+		navControl: {
+			type: Object,
+			default: () => {
+				return {
+					show: true,
+					position: 'top-right'
+				};
+			}
+		},
+		geolocateControl: {
+			type: Object,
+			default: () => {
+				return {
+					show: false,
+					position: 'top-left',
+					options: {}
+				};
+			}
+		},
+		scaleControl: {
+			type: Object,
+			default: () => {
+				return {
+					show: false,
+					position: 'top-left',
+					options: {}
+				};
+			}
+		},
+		fullscreenControl: {
+			type: Object,
+			default: () => {
+				return {
+					show: false,
+					position: 'top-right'
+				};
+			}
+		}
+	},
+	mounted () {
+		//Initialze Map
+		const map = this.mapInit();
+
+		//Add Controls to map
+		this.addControls(map);
+
+		//Register Map Events
+		this.registerEvents(map);
+	},
+	methods: {
+		mapInit () {
+			//Mapbox GL access token
+			mapboxgl.accessToken = this.accessToken;
+
+			//Add container to options object
+			this.mapOptions.container = 'map';
+
+			//New Mapbox Instance
+			const map = new mapboxgl.Map(this.mapOptions);
+
+			return map;
+		},
+		registerEvents (map) {
+			//Map Loaded
+			map.on('load', () => {
+				this.$emit('map-load', map);
+
+				//Map Mouse Move
+				map.on('mousemove', (e) => {
+					this.$emit('map-mousemove', map, e);
+				});
+
+				//Map Clicked
+				map.on('click', (e) => {
+					this.$emit('map-click', map, e);
+				});
+
+				//Map Context Menu
+				map.on('contextmenu', (e) => {
+					this.$emit('map-contextmenu', map, e);
+				});
+			});
+
+			//Map Resized
+			map.on('resize', () => {
+				this.$emit('map-resize', map);
+			});
+
+			//Map Webgl Context Lost 
+			map.on('resize', (e) => {
+				this.$emit('map-webglcontextlost', map, e);
+			});
+
+			//Map Webgl Context Restored
+			map.on('resize', (e) => {
+				this.$emit('map-webglcontextrestored', map, e);
+			});
+
+			//Map Removed
+			map.on('remove', () => {
+				this.$emit('map-remove', map);
+			});
+
+			//Map Source Data Loading
+			map.on('sourcedataloading', (e) => {
+				this.$emit('map-sourcedataloading', map, e);
+			});
+
+			//Map Touch Start
+			map.on('touchstart', (e) => {
+				this.$emit('map-touchstart', map, e);
+			});
+
+			//Map Move Start
+			map.on('movestart', (e) => {
+				this.$emit('map-movestart', map, e);
+			});
+
+			//Map Touch Move
+			map.on('movestart', (e) => {
+				this.$emit('map-movestart', map, e);
+			});
+
+			//Map Move
+			map.on('move', (e) => {
+				this.$emit('map-move', map, e);
+			});
+
+			//Map Move End
+			map.on('moveend', (e) => {
+				this.$emit('map-moveend', map, e);
+			});
+
+			//Map Error
+			map.on('error', (e) => {
+				this.$emit('map-error', map, e);
+			});
+
+			//Map Data
+			map.on('data', (e) => {
+				this.$emit('map-data', map, e);
+			});
+
+			//Map Style Data
+			map.on('styledata', (e) => {
+				this.$emit('map-styledata', map, e);
+			});
+
+			//Map Mouse Up
+			map.on('mouseup', (e) => {
+				this.$emit('map-mouseup', map, e);
+			});
+
+			//Map Touch Cancel
+			map.on('touchcancel', (e) => {
+				this.$emit('map-touchcancel', map, e);
+			});
+
+			//Map Source Data
+			map.on('sourcedata', (e) => {
+				this.$emit('map-sourcedata', map, e);
+			});
+
+			//Map Data Loading
+			map.on('dataloading', (e) => {
+				this.$emit('map-dataloading', map, e);
+			});
+
+			//Map Style Data Loading
+			map.on('styledataloading', (e) => {
+				this.$emit('map-styledataloading', map, e);
+			});
+
+			//Map Double Click
+			map.on('dblclick', (e) => {
+				this.$emit('map-dblclick', map, e);
+			});
+
+			//Map Render
+			map.on('render', () => {
+				this.$emit('map-render', map);
+			});
+
+			//Map Mouse Out
+			map.on('mouseout', (e) => {
+				this.$emit('map-mouseout', map, e);
+			});
+
+			//Map Mouse Down
+			map.on('mousedown', (e) => {
+				this.$emit('map-mousedown', map, e);
+			});
+
+			//Map Touch End
+			map.on('touchend', (e) => {
+				this.$emit('map-touchend', map, e);
+			});
+
+			//Map Zoom Start
+			map.on('zoomstart', (e) => {
+				this.$emit('map-zoomstart', map, e);
+			});
+
+			//Map Zoom End
+			map.on('zoomend', (e) => {
+				this.$emit('map-zoomend', map, e);
+			});
+
+			//Map Zoom
+			map.on('zoom', (e) => {
+				this.$emit('map-zoom', map, e);
+			});
+
+			//Map Box Zoom Cancel
+			map.on('boxzoomcancel', (e) => {
+				this.$emit('map-boxzoomcancel', map, e);
+			});
+
+			//Map Box Zoom End
+			map.on('boxzoomend', (e) => {
+				this.$emit('map-boxzoomend', map, e);
+			});
+
+			//Map Box Zoom Start
+			map.on('boxzoomstart', (e) => {
+				this.$emit('map-boxzoomstart', map, e);
+			});
+
+			//Map Rotate Start
+			map.on('rotatestart', (e) => {
+				this.$emit('map-rotatestart', map, e);
+			});
+
+			//Map Rotate
+			map.on('rotate', (e) => {
+				this.$emit('map-rotate', map, e);
+			});
+
+			//Map Rotate End
+			map.on('rotateend', (e) => {
+				this.$emit('map-rotateend', map, e);
+			});
+
+			//Map Drag End
+			map.on('dragend', (e) => {
+				this.$emit('map-dragend', map, e);
+			});
+
+			//Map Drag
+			map.on('drag', (e) => {
+				this.$emit('map-drag', map, e);
+			});
+
+			//Map Drag
+			map.on('dragstart', (e) => {
+				this.$emit('map-dragstart', map, e);
+			});
+
+			//Map Pitch
+			map.on('pitch', (e) => {
+				this.$emit('map-pitch', map, e);
+			});
+
+			//Map Pitch Start
+			map.on('pitchstart', (e) => {
+				this.$emit('map-pitchstart', map, e);
+			});
+
+			//Map Pitch End
+			map.on('pitchend', (e) => {
+				this.$emit('map-pitchend', map, e);
+			});
+
+		},
+		addControls (map) {
+			//Nav Control
+			if (this.navControl.show) {
+				const nav = new mapboxgl.NavigationControl();
+				map.addControl(nav, this.navControl.position);
+			}
+
+			//Geolocation Control
+			if (this.geolocateControl.show) {
+				const geolocate = new mapboxgl.GeolocateControl(this.geolocateControl.options);
+				map.addControl(geolocate, this.geolocateControl.position);
+			}
+
+			//Scale Control
+			if (this.scaleControl.show) {
+				const scale = new mapboxgl.ScaleControl(this.scaleControl.options);
+				map.addControl(scale, this.scaleControl.position);
+			}
+
+			//Fullscreen Control
+			if (this.fullscreenControl.show) {
+				const fullscreen = new mapboxgl.FullscreenControl();
+				map.addControl(fullscreen, this.fullscreenControl.position);
+			}
+
+		}
+	},
+};
